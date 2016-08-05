2c51b7c067e3308b1326a30a274270a941916667
Jorn Heyvaert <jorn.heyvaert@telenet.be>
2016-08-05 19:22:07 +0200

    Initial commit PrimeNumber application

--- PrimeNumbers/MainWindow.xaml.cs
+++ PrimeNumbers/MainWindow.xaml.cs
@@ -24,5 +24,33 @@ namespace PrimeNumbers
         {
             InitializeComponent();
         }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            lblLimit.Content = string.Format("Limit: {0}", Int32.MaxValue);
+            tbHelp.Text = "A prime number (or a prime) is a natural number greater than 1 that has no positive divisors other than 1 and itself.";
+        }
+
+        private void btnExecute_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                bool canContinue = !string.IsNullOrEmpty(txtPrimeNumber.Text);
+                if (canContinue)
+                {
+                    int numberToTest = Convert.ToInt32(txtPrimeNumber.Text);
+                    bool result = PrimeNumber.isPrime(numberToTest);
+                    lblResult.Content = result ? $"{numberToTest} is a prime number" : $"{numberToTest} is not a prime number";
+                }
+                else
+                {
+                    lblResult.Content = "Please input a number!!!";
+                }
+            }
+            catch (Exception)
+            {
+                lblResult.Content = "An error has occurred. Please try again!";
+            }
+        }
     }
 }
